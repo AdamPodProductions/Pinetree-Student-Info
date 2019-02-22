b2168e8b622d62f6a1776305767eb6faff2bab87
Adam Pod <adam.podoxin@gmail.com>
2019-02-21 23:08:41 -0800

    Create Course object

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
 var students = [];
-students[0] = CreateNewStudent("Adam", "Podoxin", 1265646);
+students[0] = CreateNewStudent(
+	"Adam", 
+	"Podoxin", 
+	1265646, 
+	1009, 
+	[CreateNewCourse("MSC--10H-02", "Science 10 Honours", "B207", "2", "1 (Day 1 - Day 2)", 4), 
+	CreateNewCourse("XAT--00P1-1009", "PACK", "B207", "Full Year", "6 (Day 1 - Day 2)", 0)]
+	);
 
-function CreateNewStudent(firstName, lastName, id) {
+function CreateNewStudent(firstName, lastName, id, pack, courses) {
 	var student = new Object();
 
 	student.firstName = firstName;
 	student.lastName = lastName;
 	student.id = id;
+	student.pack = pack;
+	student.courses = courses;
 
 	return student;
+}
+
+function CreateNewCourse(code, name, room, semester, block, credit) {
+	var course = new Object();
+
+	course.code = code;
+	course.name = name;
+	course.room = room;
+	course.semester = semester;
+	course.block = block;
+	course.credit = credit;
+
+	return course;
 }
 
 function GetStudentFromID(id) {
@@ -23,5 +45,5 @@ function GetStudentFromID(id) {
 
 function SubmitStudentID() {
 	var student = GetStudentFromID(document.getElementById("student-id").value);
-	alert(student.firstName + " " + student.lastName + " - " + student.id);
+	alert(student.firstName + " " + student.lastName + " - " + student.id + " " + student.courses[0].name);
 }
