8dcbe5f2acd60e53f301d6e9c21a9f8eb782d2e4
Adam Pod <adam.podoxin@gmail.com>
2019-02-22 00:03:18 -0800

    Create invalid student ID procedure

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ students[0] = CreateNewStudent(
 	1009, 
 	[
 	CreateNewCourse("XAT--00P1-1009", "PACK", "B207", "Richard, Suzanne", "Full Year", "6 (Day 1 - Day 2)", 0),
-	
+
 	CreateNewCourse("MPHED10-01", "Physical and Health Education 10", "GYM2", "Jack, Steve", "1", "1 (Day 1 - Day 2)", 4),
 	CreateNewCourse("MSS--10-02", "Social Studies 10", "A113", "Kerr, Paul", "1", "2 (Day 1 - Day 2)", 2),
-	CreateNewCourse("MFR--10-01", "French", "A212", "Mohamed Ali, Sahwa", "1", "3 (Day1 - Day 2)", 4),
+	CreateNewCourse("MFR--10-01", "French 10", "A212", "Mohamed Ali, Sahwa", "1", "3 (Day1 - Day 2)", 4),
 	CreateNewCourse("MLCE-10-04", "Career Life Education", "A115", "Fera, Natalee", "1", "4 (Day1 - Day 2)", 4),
 
 	CreateNewCourse("MSC--10H-02", "Science 10 Honours", "B207", "Richard, Suzanne", "2", "1 (Day 1 - Day 2)", 4),
@@ -62,26 +62,31 @@ function GetStudentFromID(id) {
 
 function SubmitStudentID() {
 	var student = GetStudentFromID(document.getElementById("student-id").value);
-	document.getElementById("student-id-form").style.display = "none";
-	document.getElementById("student-info").style.display = "block";
 
-	document.getElementById("name").innerHTML = "Name: " + student.firstName + " " + student.lastName;
-	document.getElementById("id").innerHTML = "Student ID: " + student.id;
-	document.getElementById("pack").innerHTML = "PACK: " + student.pack;
+	if(student == null) {
+		document.getElementById("invalid-id").style.display = "block";
+	} else {
+		document.getElementById("student-id-form").style.display = "none";
+		document.getElementById("student-info").style.display = "block";
 
-	var i = 0;
-	for(i = 0; i < student.courses.length; i++) {
-		var row = document.getElementById("courses").insertRow(i + 1);
+		document.getElementById("name").innerHTML = "Name: " + student.firstName + " " + student.lastName;
+		document.getElementById("id").innerHTML = "Student ID: " + student.id;
+		document.getElementById("pack").innerHTML = "PACK: " + student.pack;
 
-		row.insertCell(0).innerHTML = student.courses[i].code;
-		row.insertCell(1).innerHTML = student.courses[i].name;
-		row.insertCell(2).innerHTML = student.courses[i].room;
-		row.insertCell(3).innerHTML = student.courses[i].teacher;
-		row.insertCell(4).innerHTML = student.courses[i].semester;
-		row.insertCell(5).innerHTML = student.courses[i].block;
-		row.insertCell(6).innerHTML = student.courses[i].credit;
+		var i = 0;
+		for(i = 0; i < student.courses.length; i++) {
+			var row = document.getElementById("courses").insertRow(i + 1);
+
+			row.insertCell(0).innerHTML = student.courses[i].code;
+			row.insertCell(1).innerHTML = student.courses[i].name;
+			row.insertCell(2).innerHTML = student.courses[i].room;
+			row.insertCell(3).innerHTML = student.courses[i].teacher;
+			row.insertCell(4).innerHTML = student.courses[i].semester;
+			row.insertCell(5).innerHTML = student.courses[i].block;
+			row.insertCell(6).innerHTML = student.courses[i].credit;
+		}
+
+		document.getElementById("locker-number").innerHTML = "Locker number: " + student.lockerNumber;
+		document.getElementById("locker-combo").innerHTML = "Locker combination: " + student.lockerCombo;
 	}
-
-	document.getElementById("locker-number").innerHTML = "Locker number: " + student.lockerNumber;
-	document.getElementById("locker-combo").innerHTML = "Locker combination: " + student.lockerCombo;
 }
